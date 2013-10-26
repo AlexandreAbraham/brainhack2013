c320777937e65d85b09585698fb8a7bcb0fe4fe4
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-10-26 10:27:37 +0200

    For the pretty Solveig <3

--- plot_craddock_spectral_clustering.py
+++ plot_craddock_spectral_clustering.py
@@ -1,13 +1,17 @@
 from brainhack.datasets import fetch_craddock_2012_test
+from nilearn.input_data import NiftiMasker
 from joblib import Memory
 from sklearn.feature_extraction.image import grid_to_graph
 import nibabel
 from sklearn.cluster import spectral_clustering
 from scipy.sparse import coo_matrix
 import numpy as np
+from os.path import exists
 
 
 dataset = fetch_craddock_2012_test()
+masker = NiftiMasker(mask=dataset.mask)
+masker.fit()
 
 ### Connectivity graph #######################################################
 
@@ -23,19 +27,60 @@ connectivity = grid_to_graph(*mask_data.shape, mask=mask_data)
 from brainhack.covariance.pearson import PearsonCorrelation
 
 pearson = PearsonCorrelation(spatial=False)
+pearson_spatial = PearsonCorrelation(spatial=True)
 
 ### Compute similarity matrices ##############################################
 from brainhack.covariance.multi_covariance import MultiCovariance
 
-multi_cov = MultiCovariance(pearson, mask=dataset.mask, standardize=True,
-        detrend=True, memory=Memory(cachedir='nilearn_cache'), memory_level=1)
-# Should add low_pass = 0.1 ?
+if not exists('covariances.npy'):
 
-multi_cov.fit(dataset.func, connectivity=connectivity)
+    multi_cov = MultiCovariance(pearson, mask=dataset.mask, standardize=True,
+        detrend=True, memory=Memory(cachedir='nilearn_cache'), memory_level=1)
+    # Should add low_pass = 0.1 ?
+
+    multi_cov.fit(dataset.func, connectivity=connectivity)
+    np.save('covariances.npy', multi_cov.covariances_)
+
+covariances = np.load('covariances.npy')
+
+if not exists('covariances_spatial.npy'):
+
+    multi_cov_spatial = MultiCovariance(pearson_spatial, mask=dataset.mask,
+        standardize=True, detrend=True,
+        memory=Memory(cachedir='nilearn_cache'), memory_level=1)
+
+    multi_cov_spatial.fit(dataset.func, connectivity=connectivity)
+    np.save('covariances_spatial.npy', multi_cov_spatial.covariances_)
+
+covariances_spatial = np.load('covariances_spatial.npy')
+
+
+### Reproduce figure #1 of Craddock paper ####################################
+import pylab as pl
+
+rt = np.hstack([c.data for c in covariances])
+rs = np.hstack([c.data for c in covariances_spatial])
+
+# Split points that have same signs and opposite signs
+rr = rt * rs
+rt_plus = rt[rr >= 0]
+rs_plus = rs[rr >= 0]
+rt_minus = rt[rr < 0]
+rs_minus = rs[rr < 0]
+
+pl.figure(figsize=(8, 8))
+
+pl.scatter(rt_plus, rs_plus)
+pl.scatter(rt_minus, rs_minus, c='r')
+
+
+pl.xlim(-1., 1.)
+pl.ylim(-1., 1.)
+
+pl.show()
 
 ### First group strategy: simply average similarity matrices #################
-'''
-group_similarity_matrix = np.mean(multi_cov.covariances_, axis=0)
+group_similarity_matrix = np.mean(covariances, axis=0)
 row, col = group_similarity_matrix.nonzero()
 
 # Threshold the matrix
@@ -46,9 +91,9 @@ group_similarity_matrix = coo_matrix((data, (row, col)))
 # Run clustering
 group_maps = spectral_clustering(group_similarity_matrix,
         n_clusters=100, assign_labels='discretize')
-nibabel.save(multi_cov.masker_.inverse_transform(group_maps + 1),
+nibabel.save(masker.inverse_transform(group_maps + 1),
              'group_maps_1.nii.gz')
-'''
+
 ### Second group strategy ####################################################
 
 
@@ -66,7 +111,7 @@ def clustering_to_connectivity(labels):
     return coo_matrix((np.ones(len(r)), (r, c)))
 
 clusters = []
-for i, cov in enumerate(multi_cov.covariances_):
+for i, cov in enumerate(covariances):
     # Threshold covariance
     data = cov.data
     data[data < .5] = 0.
@@ -86,5 +131,5 @@ group_similarity_matrix = coo_matrix((data, (row, col)))
 # Run clustering
 group_maps = spectral_clustering(group_similarity_matrix,
         n_clusters=100, assign_labels='discretize')
-nibabel.save(multi_cov.masker_.inverse_transform(group_maps + 1),
+nibabel.save(masker.inverse_transform(group_maps + 1),
              'group_maps_2.nii.gz')
